39d77d46530c2d540c703a210daa027abf41a6ea
Falko Zurell <falko.zurell@ubirch.com>
2016-04-20 15:55:16 +0200

    add a bit more verbosity to build
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Building container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
+
 mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} \
   --build-arg JAVA_UPDATE=${JAVA_UPDATE:=77} \
   --build-arg JAVA_BUILD=${JAVA_BUILD:=03} \
