4199b7fde0fdf13fe2330473222bd7a5d62b06fd
Falko Zurell <falko.zurell@ubirch.com>
2016-04-21 09:10:46 +0200

    added new label calculation from go-tools

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
+# check out out GO tools
+git clone https://github.com/ubirch/go-tools.git
+
+$if [ $? -ne 0 ]; then exit 1
+
+NEW_LABEL=`./go-tools/concat-labels.sh`
+
 echo "Building container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
 
 mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} \
   --build-arg JAVA_UPDATE=${JAVA_UPDATE:=77} \
   --build-arg JAVA_BUILD=${JAVA_BUILD:=03} \
-  -t ubirch/java .
+  -t ubirch/java:v${NEW_LABEL} .
   if [ $? -eq 0 ]; then
     echo ${JAVA_VERSION:=8} > VAR/JAVA_VERSION
     echo ${JAVA_UPDATE:=77} > VAR/JAVA_UPDATE
